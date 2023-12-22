9340d5917c0122aa308f2d9ed0415c11a3333948
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-12-22 13:50:35 -0800

    Redirect invalid paths to home

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Projects from './Projects';
 import Resume from './Resume';
 import Contact from "./Contact";
 import MyNavbar from "./MyNavbar";
 import Home from "./Home";
+import Scoreboard from "./ProjectsPages/Scoreboard"
 
 function App() {
     return (
@@ -15,8 +16,11 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home/>}></Route>
                     <Route path="/projects" element={<Projects/>}></Route>
+                    <Route path="/projects/scoreboard" element={<Scoreboard/>}></Route>
                     <Route path="/resume" element={<Resume/>}></Route>
                     <Route path="/contact" element={<Contact/>}></Route>
+                    <Route path = "*" element={<Navigate to="/" />} />
+
                 </Routes>
             </div>
         </Router>
