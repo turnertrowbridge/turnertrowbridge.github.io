8dbf1a420b7d94d661ed66ad1f3d809b003677a6
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-12-22 15:43:31 -0800

    Added github repo fetching and scoreboard project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import '../styles/Projects.css';
-import projectsData from "./ProjectsPages/projects.json";
+import repoInfo from './ProjectsPages/repo_info.json'; // Importing the JSON file
+import favRepos from './ProjectsPages/fav_repos.json';
 
- function Projects() {
+function Projects() {
+    const [projects, setProjects] = useState([]);
 
-     return (
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const username = repoInfo[0].username; // Assuming 'username' is the key in repo_info.json
+                const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated`);
+                if (response.ok) {
+                    const data = await response.json();
+
+
+                    const favoriteRepos = data.filter(repo => favRepos.includes(repo.name));
+                    const remainingRepos = data.filter(repo => !favRepos.includes(repo.name));
+
+                    const mergedRepos = [...favoriteRepos, ...remainingRepos];
+                    console.log(mergedRepos);
+                    setProjects(mergedRepos);
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    function formatDate(dateStr) {
+        const date = new Date(dateStr);
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Month (0-11) => (01-12)
+        const day = String(date.getDate()).padStart(2, '0'); // Day of the month (1-31)
+        const year = date.getFullYear(); // Year
+        return `${month}/${day}/${year}`;
+    }
+
+    return (
         <div className="container text-center projects-container">
             <div className="row justify-content-start">
-                {projectsData.map((project) => (
+                {projects.map((project) => (
                     <div key={project.id} className="col-md-4 mb-3">
                         <Card className="project-card d-inline-block br-3 p-3 m-2 border border-2 shadow-lg h-100">
-                            <Card.Img
-                                variant="top"
-                                src={project.image}
-                                alt={project.title}
-                                className="project-image"
-                            />
                             <Card.Body>
-                                <Card.Title>{project.title}</Card.Title>
-                                <Card.Text>{project.text}</Card.Text>
-                                <Button variant="primary" href={project.link} target="_blank">
+                                <Card.Title>{project.name}</Card.Title>
+                                <Card.Text>{project.description}</Card.Text>
+                                <p>{formatDate(project.updated_at)}</p>
+                                <Button variant="primary" href={project.html_url} target="_blank" rel="noopener noreferrer">
                                     Go to Project
                                 </Button>
                             </Card.Body>
@@ -30,6 +61,6 @@ import projectsData from "./ProjectsPages/projects.json";
             </div>
         </div>
     );
- }
+}
 
- export default Projects;
+export default Projects;
---
+++ src/components/ProjectsPages/Scoreboard.js
@@ -0,0 +1,55 @@
+import React, {useState, useEffect} from 'react';
+import {ListGroup} from 'react-bootstrap';
+import repoInfo from './repo_info.json';
+
+const Scoreboard = () => {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const fetchedRepos = await Promise.all(
+                    repoInfo.map(async ({username, repo}) => {
+                        const response = await fetch(`https://api.github.com/repos/${username}/${repo}`);
+                        if (response.ok) {
+                            const data = await response.json();
+                            console.log(data);
+                            return {
+                                id: data.id,
+                                title: data.name,
+                                languages: data.language,
+                                description: data.description,
+                                url: data.html_url,
+                                lastUpdated: data.updated_at,
+                            };
+                        } else {
+                            throw new Error('Failed to fetch data');
+                        }
+                    })
+                );
+                setRepos(fetchedRepos);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <ListGroup>
+            {repos.map((repo) => (
+                <ListGroup.Item key={repo.id}>
+                    <a href={repo.url} target="_blank" rel="noopener noreferrer">
+                        <h2>{repo.title}</h2>
+                    </a>
+                    <p>Language: {repo.languages}</p>
+                    {repo.description && <p>Description: {repo.description}</p>}
+                    <p>Last Updated: {new Date(repo.lastUpdated).toLocaleString()}</p>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    );
+};
+
+export default Scoreboard;
