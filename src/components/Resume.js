3f220b0f8c31a797ff1328acf244a0860c599af7
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2023-12-27 18:01:41 -0800

    Fixed resume page somewhat

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,13 +9,17 @@ function Resume() {
     }, []);
 
     return (
-        <div className="resume-container">
-            <Iframe
-                url="https://docs.google.com/document/d/e/2PACX-1vTn1njfPzGFr1oTufjfbMrciGG0Ki2Yx8LX17UhrNlr8Lge824pIu3l1h0UMmHcdT3cqAs1orF1lpQc/pub?embedded=true"
-                width="100"
-                height="800px"
-                className="resume-iframe"
-            />
+        <div className="center">
+            <div className="resume-container">
+                <Iframe
+                    url="https://docs.google.com/document/d/e/2PACX-1vTn1njfPzGFr1oTufjfbMrciGG0Ki2Yx8LX17UhrNlr8Lge824pIu3l1h0UMmHcdT3cqAs1orF1lpQc/pub?embedded=true"
+                    title="Turner's Resume Google Doc"
+                    width="100"
+                    height="800em"
+                    className="resume-iframe"
+                    scrolling="yes"
+                />
+            </div>
         </div>
     );
 }
