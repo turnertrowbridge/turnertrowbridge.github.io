6e02026bb3ad857149c50e47043b7a172e86d0a3
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-02-19 19:51:39 -0800

    Added AWS Workout Project to favorites

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,26 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Iframe from 'react-iframe';
 import "../styles/Resume.css"
 
 function Resume() {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
     useEffect(() => {
         document.title = "Resume - Turner's Website";
+
+        const handleResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
+
+    const iframeHeight = windowHeight * 0.8;
+    const iframeTopOffset = windowHeight * 0.3;
 
     return (
         <div className="center">
@@ -15,7 +29,8 @@ function Resume() {
                     url="https://docs.google.com/document/d/e/2PACX-1vTn1njfPzGFr1oTufjfbMrciGG0Ki2Yx8LX17UhrNlr8Lge824pIu3l1h0UMmHcdT3cqAs1orF1lpQc/pub?embedded=true"
                     title="Turner's Resume Google Doc"
                     width="100"
-                    height="800em"
+                    height={`${iframeHeight}px`}
+                    style={{ marginTop: `${iframeTopOffset}px` }}
                     className="resume-iframe"
                     scrolling="yes"
                 />
